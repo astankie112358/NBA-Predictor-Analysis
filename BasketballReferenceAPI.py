0a9b6084e421e1048b9ffeb21f8e1a8caf10e0ae
Adam <astankie@wp.pl>
2024-11-16 12:08:22 +0100

    Working data file to file

--- BasketballReferenceAPI.py
+++ BasketballReferenceAPI.py
@@ -1,34 +1,55 @@
+import os
+
 import Teams
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, date
 from basketball_reference_scraper.box_scores import get_box_scores
 from basketball_reference_scraper.seasons import get_schedule
+
 
 class BasketballReferenceAPI:
     def __init__(self):
         self.team_abb = Teams.TeamAbb
 
     def get_game(self, date, team1_abb, team2_abb):
-        game_id=team1_abb+team2_abb+date.strftime('%Y%m%d')
+        game_id = team1_abb + team2_abb + date.strftime('%Y%m%d')
         box_score = pd.DataFrame()
-        for type in ['BASIC','ADVANCED']:
+        for type in ['BASIC', 'ADVANCED']:
             new_box_score = get_box_scores(date.strftime('%Y-%m-%d'), team1_abb, team2_abb,
-                                   period='GAME', stat_type=type)
-            new_box_score=pd.concat([new_box_score[team1_abb], new_box_score[team2_abb]],keys=[team1_abb, team2_abb])
-            box_score=box_score.append(new_box_score)
-        box_score['GAME_ID']=game_id
+                                           period='GAME', stat_type=type)
+            new_box_score = pd.concat([new_box_score[team1_abb], new_box_score[team2_abb]], keys=[team1_abb, team2_abb])
+            box_score = box_score.append(new_box_score)
+        box_score['GAME_ID'] = game_id
         print(game_id)
-        return box_score
+        return box_score, game_id
 
     def get_season_box_scores(self, season):
-        schedule = pd.DataFrame.from_dict(get_schedule(season, False))
-        box_score=pd.DataFrame()
-        for game in schedule.iterrows():
-            home_team_abb = Teams.TeamAbb[game[1]['HOME'].upper()]
-            visitor_team_abb = Teams.TeamAbb[game[1]['VISITOR'].upper()]
-            new_box_score = self.get_game(game[1]['DATE'], visitor_team_abb, home_team_abb)
-            pd.concat([box_score,new_box_score])
+        schedule = pd.DataFrame.from_dict(get_schedule(season, True))
+        box_score = pd.DataFrame()
+        for index, game in schedule.iterrows():
+            print('Game: ' + str(index + 1) + ' out of ' + str(len(schedule) + 1))
+            home_team_abb = Teams.TeamAbb[game['HOME'].upper()]
+            visitor_team_abb = Teams.TeamAbb[game['VISITOR'].upper()]
+            new_box_score = self.get_game(game['DATE'], home_team_abb, visitor_team_abb)[0]
+            pd.concat([box_score, new_box_score])
         return box_score
-aaa=BasketballReferenceAPI()
-aaa.get_season_box_scores(2024).to_csv('file1.csv')
 
+    def get_season_box_scores_separtate_file(self, season, path):
+        schedule = pd.DataFrame.from_dict(get_schedule(season, False))
+        schedule['HOME'] = schedule.apply(lambda row: Teams.TeamAbb[row['HOME'].upper()], axis=1)
+        schedule['VISITOR'] = schedule.apply(lambda row: Teams.TeamAbb[row['VISITOR'].upper()], axis=1)
+        schedule['GAME_ID'] = schedule['HOME'] + schedule['VISITOR'] + schedule['DATE'].dt.strftime('%Y%m%d')
+        files = os.listdir(path)
+        files=[file.replace('.csv','') for file in files]
+        schedule.drop(schedule[schedule['GAME_ID'].isin(files)].index, inplace=True)
+        box_score = pd.DataFrame()
+        for index, game in schedule.iterrows():
+            print('Game: ' + str(index + 1) + ' out of ' + str(len(schedule) + 1))
+            new_box_score = self.get_game(game['DATE'], game['HOME'], game['VISITOR'])[0]
+            box_score = pd.concat([box_score, new_box_score])
+            box_score.to_csv(path + game['GAME_ID'] + '.csv')
+        return path
+
+
+basketball_reference_api = BasketballReferenceAPI()
+basketball_reference_api.get_season_box_scores_separtate_file(2023, 'D:\\Users\\Adam\\PycharmProjects\\NBA_GAMES\\')
