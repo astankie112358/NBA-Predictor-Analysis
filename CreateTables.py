be54346d80bb04f398ebfd019ded38cc7ff4db12
Adam <astankie@wp.pl>
2025-02-16 16:16:56 +0100

    Working load to stg for boxscores

--- CreateTables.py
+++ CreateTables.py
@@ -1,57 +1,57 @@
-from sqlalchemy import Column, Integer, text
+from sqlalchemy import Column, Integer, text, MetaData, Table
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.dialects.postgresql import VARCHAR
 from sqlalchemy import create_engine
+from sqlalchemy.orm import registry, mapper
 import os
 
-Base = declarative_base()
+metadata = MetaData()
+metadata = MetaData(schema='stg')
 
-class PlayersStats(Base):
-    __tablename__ = 'stg_Boxscores'
-    __table_args__ = {'schema': 'stg'}
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    season = Column(VARCHAR(6))
-    unnamed_0 = Column('Unnamed: 0', VARCHAR(3))
-    unnamed_1 = Column('Unnamed: 1', Integer)
-    player = Column('PLAYER', VARCHAR(60))
-    mp = Column('MP', VARCHAR(12))
-    fg = Column('FG', VARCHAR(12))
-    fga = Column('FGA', VARCHAR(12))
-    fg_percent = Column('FG%', VARCHAR(12))
-    three_p = Column('3P', VARCHAR(12))
-    three_pa = Column('3PA', VARCHAR(12))
-    three_p_percent = Column('3P%', VARCHAR(12))
-    ft = Column('FT', VARCHAR(12))
-    fta = Column('FTA', VARCHAR(12))
-    ft_percent = Column('FT%', VARCHAR(12))
-    orb = Column('ORB', VARCHAR(12))
-    drb = Column('DRB', VARCHAR(12))
-    trb = Column('TRB', VARCHAR(12))
-    ast = Column('AST', VARCHAR(12))
-    stl = Column('STL', VARCHAR(12))
-    blk = Column('BLK', VARCHAR(12))
-    tov = Column('TOV', VARCHAR(12))
-    pf = Column('PF', VARCHAR(12))
-    pts = Column('PTS', VARCHAR(12))
-    gmsc = Column('GmSc', VARCHAR(12))
-    plus_minus = Column('+/-', VARCHAR(12))
-    ts_percent = Column('TS%', VARCHAR(12))
-    efg_percent = Column('eFG%', VARCHAR(12))
-    threepar = Column('3PAr', VARCHAR(12))
-    ftr = Column('FTr', VARCHAR(12))
-    orb_percent = Column('ORB%', VARCHAR(12))
-    drb_percent = Column('DRB%', VARCHAR(12))
-    trb_percent = Column('TRB%', VARCHAR(12))
-    ast_percent = Column('AST%', VARCHAR(12))
-    stl_percent = Column('STL%', VARCHAR(12))
-    blk_percent = Column('BLK%', VARCHAR(12))
-    tov_percent = Column('TOV%', VARCHAR(12))
-    usg_percent = Column('USG%', VARCHAR(12))
-    ortg = Column('ORtg', VARCHAR(12))
-    drtg = Column('DRtg', VARCHAR(12))
-    bpm = Column('BPM', VARCHAR(12))
-    game_id = Column('GAME_ID', VARCHAR(14))
-
+# Define the table using the metadata object
+players_stats = Table(
+    'stg_Boxscores', metadata,
+    Column('Unnamed: 0', VARCHAR(3)),
+    Column('Unnamed: 1', Integer),
+    Column('PLAYER', VARCHAR(60)),
+    Column('MP', VARCHAR(13)),
+    Column('FG', VARCHAR(13)),
+    Column('FGA', VARCHAR(13)),
+    Column('FG%', VARCHAR(13)),
+    Column('3P', VARCHAR(13)),
+    Column('3PA', VARCHAR(13)),
+    Column('3P%', VARCHAR(13)),
+    Column('FT', VARCHAR(13)),
+    Column('FTA', VARCHAR(13)),
+    Column('FT%', VARCHAR(13)),
+    Column('ORB', VARCHAR(13)),
+    Column('DRB', VARCHAR(13)),
+    Column('TRB', VARCHAR(13)),
+    Column('AST', VARCHAR(13)),
+    Column('STL', VARCHAR(13)),
+    Column('BLK', VARCHAR(13)),
+    Column('TOV', VARCHAR(13)),
+    Column('PF', VARCHAR(13)),
+    Column('PTS', VARCHAR(13)),
+    Column('GmSc', VARCHAR(13)),
+    Column('+/-', VARCHAR(13)),
+    Column('TS%', VARCHAR(13)),
+    Column('eFG%', VARCHAR(13)),
+    Column('3PAr', VARCHAR(13)),
+    Column('FTr', VARCHAR(13)),
+    Column('ORB%', VARCHAR(13)),
+    Column('DRB%', VARCHAR(13)),
+    Column('TRB%', VARCHAR(13)),
+    Column('AST%', VARCHAR(13)),
+    Column('STL%', VARCHAR(13)),
+    Column('BLK%', VARCHAR(13)),
+    Column('TOV%', VARCHAR(13)),
+    Column('USG%', VARCHAR(13)),
+    Column('ORtg', VARCHAR(13)),
+    Column('DRtg', VARCHAR(13)),
+    Column('BPM', VARCHAR(13)),
+    Column('GAME_ID', VARCHAR(14))
+)
 
 db_username = 'nba_admin'
 db_password = os.getenv('NBA_ADMIN_PASS')
@@ -64,4 +64,4 @@ with engine.connect() as connection:
     connection.execute(text("CREATE SCHEMA IF NOT EXISTS stg"))
     connection.commit()
 
-Base.metadata.create_all(engine)
+metadata.create_all(engine)
--- LoadToStaging.py
+++ LoadToStaging.py
@@ -8,21 +8,25 @@ from sqlalchemy.orm import sessionmaker
 
 class DatabaseLoader:
     def __init__(self):
-        db_username = 'Admin'
+        db_username = 'nba_admin'
         db_password = os.getenv('NBA_ADMIN_PASS')
         db_host = 'localhost'
         db_port = '5432'
         db_name = 'NBA_Analysis'
-        #self.engine = create_engine(f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
+        self.engine = create_engine(f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}')
     def get_files_to_load(self,path):
         files = os.listdir(path)
         for file_name in files:
             file=pandas.read_csv(path+'\\'+file_name,low_memory=False)
-            for column in file.columns:
-                if pd.api.types.is_string_dtype(file[column]):
-                    print(column," varchar ",file[column].str.len().max())
-                else:
-                    print(column, "int")
+            with self.engine.connect() as connection:
+                transaction = connection.begin()
+                try:
+                    file.to_sql('stg_Boxscores', connection, if_exists='append', index=False, schema='stg',index_label='id')
+                    transaction.commit()
+                except:
+                    transaction.rollback()
+                    raise
+            break
 
 
 databaseLoader = DatabaseLoader()
